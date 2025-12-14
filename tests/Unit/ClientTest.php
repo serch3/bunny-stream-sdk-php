108c0a1c74b55c45e94bbc495ebc20fb88855536
serch3 <alwovive@gmail.com>
2025-12-14 10:08:17 -0600

    Add ClientTest.php for testing Client instantiation

---
+++ tests/Unit/ClientTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Bunny\Stream\Client;
+use Bunny\Stream\API\Video;
+use Bunny\Stream\API\Collection;
+use Bunny\Stream\API\Livestream;
+use Bunny\Stream\API\Statistics;
+use Bunny\Stream\API\Tus;
+use GuzzleHttp\Client as GuzzleClient;
+
+describe('Client', function () {
+    it('can be instantiated', function () {
+        $client = new Client('api-key', '123');
+        expect($client)->toBeInstanceOf(Client::class);
+    });
+
+    it('exposes API instances', function () {
+        $client = new Client('api-key', '123');
+
+        expect($client->video())->toBeInstanceOf(Video::class)
+            ->and($client->collection())->toBeInstanceOf(Collection::class)
+            ->and($client->livestream())->toBeInstanceOf(Livestream::class)
+            ->and($client->statistics())->toBeInstanceOf(Statistics::class)
+            ->and($client->tus())->toBeInstanceOf(Tus::class);
+    });
+
+    it('can accept custom guzzle client', function () {
+        $guzzle = new GuzzleClient(['base_uri' => 'http://custom.com']);
+        $client = new Client('api-key', '123', $guzzle);
+        
+        expect($client)->toBeInstanceOf(Client::class);
+    });
+});
