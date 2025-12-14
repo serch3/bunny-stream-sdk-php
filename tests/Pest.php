7cab5b67d32fac2a816942e01227d68fb4b4862f
serch3 <alwovive@gmail.com>
2025-12-14 09:36:34 -0600

    Add Pest.php for unit testing with GuzzleHttp mock client

---
+++ tests/Pest.php
@@ -0,0 +1,39 @@
+<?php
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+
+/*
+|--------------------------------------------------------------------------
+| Test Case
+|--------------------------------------------------------------------------
+|
+| The closure you provide to your test functions is always bound to a specific PHPUnit test
+| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
+| need to change it using the "uses()" function to bind a different classes or traits.
+|
+*/
+
+uses(PHPUnit\Framework\TestCase::class)->in('Unit');
+
+/*
+|--------------------------------------------------------------------------
+| Helpers
+|--------------------------------------------------------------------------
+*/
+
+function mockGuzzleClient(array $responses, array &$container): GuzzleClient
+{
+    $history = Middleware::history($container);
+    $mock = new MockHandler($responses);
+
+    $handlerStack = HandlerStack::create($mock);
+    $handlerStack->push($history);
+
+    return new GuzzleClient([
+        'handler' => $handlerStack,
+        'base_uri' => 'https://video.bunnycdn.com/library/123/',
+    ]);
+}
